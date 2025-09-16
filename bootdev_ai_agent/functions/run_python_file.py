e1cc2b8c68bfc1fb16bad5c9645e36ccb9fde719
darkwulf <tech.darkwulf@gmail.com>
2025-09-16 18:55:59 +0200

    Third part of the course

--- bootdev_ai_agent/functions/run_python_file.py
+++ bootdev_ai_agent/functions/run_python_file.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+from google.genai import types
 
 def run_python_file(working_directory, file_path, args=[]):
     working_dir_abs = os.path.abspath(working_directory)
@@ -28,3 +29,26 @@ def run_python_file(working_directory, file_path, args=[]):
             return "\n".join(parts)
     except Exception as e:
         return f"Error: executing Python file: {e}"
+    
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a Python file within the working directory and returns the output from the interpreter.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the Python file to execute, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(
+                    type=types.Type.STRING,
+                    description="Optional arguments to pass to the Python file.",
+                ),
+                description="Optional arguments to pass to the Python file.",
+            ),
+        },
+        required=["file_path"]
+    ),
+)
--- bootdev_ai_agent/main.py
+++ bootdev_ai_agent/main.py
@@ -4,31 +4,67 @@ from google import genai
 import sys
 from google.genai import types
 import argparse
+from config import *
+from call_function import available_functions, call_function
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
 
-client = genai.Client(api_key=api_key)
+    parser = argparse.ArgumentParser(description="An AI agent CLI tool.")
+    parser.add_argument("prompt", type=str, help="The user's prompt for the AI agent.")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output.")
+    args = parser.parse_args()
+
+    user_prompt = args.prompt
+    is_verbose = args.verbose
+
+    if len(sys.argv) == 1:
+        print("error: no prompt given")
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if is_verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
+
+    generate_content(client, messages, is_verbose)
+
+def generate_content(client, messages, is_verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
+    if is_verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if not response.function_calls:
+        return response.text 
+    
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, is_verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if is_verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
 
 
-if len(sys.argv) == 1:
-    print("error: no prompt given")
-    sys.exit(1)
-messages = [
-    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
-]
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
-parser = argparse.ArgumentParser(description="An AI agent CLI tool.")
-parser.add_argument("prompt", type=str, help="The user's prompt for the AI agent.")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output.")
-args = parser.parse_args()
-
-user_prompt = args.prompt
-is_verbose = args.verbose
-
-print(response.text)
-if is_verbose:
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if __name__ == "__main__":
+    main()
